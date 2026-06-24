b36d74b259c169b678a4c07e6dad381e309046a9
techmuse <44119098+techmuse8@users.noreply.github.com>
2026-06-24 19:29:49 -0400

    add render hook
    The render entrypoint I found so far only lasted as long as the scene is active; The textbox as a result ends up flickering for a bit during scene transitions, will have to find a better entrypoint then I suppose

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include "hk/gfx/DebugRenderer.h"
+#include "hk/hook/InstrUtil.h"
 #include "hk/hook/Trampoline.h"
+#include "hk/ro/RoUtil.h"
 #include "hk/svc/api.h"
 
 #include "nn/fs.h"
+#include "hk/nvn/nvn_CppMethods.h"
 
 HkTrampoline sdFileCreate = [](TrampolineStatic(), void* a1) -> void {
     orig(a1);
@@ -16,16 +19,55 @@ HkTrampoline LMS_GetTextHook = [](TrampolineStatic(), void* a1, int a2) -> int {
     return orig(a1, 5);
 };
 
-// Runs every frame
-HkTrampoline testHook = [](TrampolineStatic(), int a1) -> void {
-    orig(a1);
-    hk::svc::OutputDebugString("Hi!\n", 5);
+struct someRenderClass {
+    char unk_0[0xb0 - 0x0];
+    nvn::CommandBuffer* commandBuffer;
+    // ...
+
+    void endCommandBufferRecording();
 };
 
-extern "C" void hkMain() {
-    testHook.installAtMainOffset(0x509e10);
-    sdFileCreate.installAtOffset(hk::ro::getMainModule(), 0x50c9c0);
-    LMS_GetTextHook.installAtMainOffset(0x4f0ba0);
+// Runs every frame
+void renderHook(someRenderClass* _this) {
+   // defer {ret = cmdBuf->EndRecording(); };
 
-   // hk::gfx::DebugRenderer::instance()->installHooks();
+   // hk::svc::OutputDebugString("Hi!\n", 5);
+
+    /* DebugRenderer */
+    auto* renderer = hk::gfx::DebugRenderer::instance();
+
+    renderer->clear();
+    renderer->begin(_this->commandBuffer);
+
+    renderer->setGlyphSize(0.45);
+
+    renderer->drawQuad(
+        { { 30, 30 }, { 0, 0 }, 0xef000000 },
+        { { 300, 30 }, { 1.0, 0 }, 0xef000000 },
+        { { 300, 100 }, { 1.0, 1.0 }, 0xef000000 },
+        { { 30, 100 }, { 0, 1.0 }, 0xef000000 });
+
+    renderer->setCursor({ 50, 50 });
+    renderer->printf("Rhythm ReGrooved\n\n by techmuse, et al.");
+
+    renderer->end();
+
+    _this->endCommandBufferRecording();
+
+   // return _this->commandBuffer->EndRecording();
+
 }
+
+
+
+extern "C" void hkMain() {
+
+
+    sdFileCreate.installAtMainOffset(0x50c9c0);
+   // LMS_GetTextHook.installAtMainOffset(0x4f0ba0);
+    hk::hook::writeBranchLink(hk::ro::getMainModule(), 0x51098c, &renderHook);
+
+    hk::gfx::DebugRenderer::instance()->installHooks();
+}
+
+
